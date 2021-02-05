0a7f5caee9fe4d0a691ff36cbb8cec0a930e1059
June P <TheyAreJune@gmail.com>
2021-02-05 02:37:50 -0500

    Add Icon Folder and Example Content

---
+++ Example Project/Final Segment/Published-Result.md
@@ -0,0 +1,35 @@
+Logo image up at the top, along with some nice message.
+
+The published video has now been posted to [social media platorm(s)]. Links are provided below.
+
+- YouTube
+- Instagram Teaser
+- Twitter Teaser
+- Vimeo(?)
+
+Special Thanks to [Example User] for recommending this topic and working with me to develop the initial scripts of this video.
+
+Upcoming topic ideas for the future:
+- [Example 1]
+- [Example 2]
+
+Top comment from last publish:
+> [Some nice comment or funny comment]
+
+---
+
+Bibliography:
+- [Citation 1]
+- [Citation 2]
+
+---
+
+Software Used:
+- Manim CE
+- Blender
+- VS Code
+- Atom
+- Audacity
+- Ableton Live 10 Lite
+- LMMS
+- All of these are examples
---
+++ Example Project/Rough Animation/Rough-2-5.py
@@ -0,0 +1,49 @@
+from manim import *
+import math
+
+class CasesExample(Scene):
+    def construct(self):
+        text = MathTex(
+            r"H_n(a, b)=a[n]b="
+            r"\begin{cases}"
+            r"b+1, & \text{if } n=0 \\"
+            r"a, & \text{if } n=1 \text{ and } b=0 \\"
+            r"0, & \text{if } n=2 \text{ and } b=0 \\"
+            r"1, & \text{if } n \geq 3 \text{ and } b=0 \\"
+            r"H_{n-1}(a,H_n(a,b-1)), & \text{otherwise}"
+            r"\end{cases}"
+        ).scale(0.85)
+
+        text1 = MathTex(
+            r"H_n(a, b)=a[n]b="
+            r"\begin{cases}"
+            r"b+1, & \text{if } n=0 \\"
+            r"a, & \text{if } n=1 \text{ and } b=0 \\"
+            r"0, & \text{if } n=2 \text{ and } b=0 \\"
+            r"1, & \text{if } n \geq 3 \text{ and } b=0 \\"
+            r"H_{n-1}(a,H_n(a,b-1)), & \text{otherwise}"
+            r"\end{cases}"
+        ).scale(0.85)
+
+        eq = MathTex(
+            "=",  #0
+            r"a[n-1](a[n-1](a[n-1](\cdots [n-1](a[n-1](a[n-1]a \cdots))),",  #1
+            r"\ \  n \geq 2"  #2
+        ).scale(0.85)
+
+        text1.move_to(UP)
+        eq.next_to(text1, DOWN*3)
+        # title = MathTex(r"\text{The Recursive Definition of } \mathbb{N}_0 \text{:}")
+        # title.next_to(text, UP)
+        self.play(DrawBorderThenFill(text))
+        self.play(Transform(text, text1))
+        self.play(DrawBorderThenFill(eq))
+
+        brace = Brace(eq[1], DOWN, buff=SMALL_BUFF)
+        t1 = brace.get_text("b copies of a")
+
+        self.play(
+            GrowFromCenter(brace),
+            FadeIn(t1)
+        )
+        # self.play(Write(title))
